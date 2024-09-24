c4ffc63134d906868e78864aff48ff2cd8a13fa1
fadhil riyanto <me@fadev.org>
2024-09-24 08:32:44 +0700

    add getopt

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
-#include <stdio.h>
 
-int main()
+#include <stdio.h>
+#include <getopt.h>
+#include "submodule/log.c-patched/src/log.h"
+
+#define dbgchr(x) log_info("%c", x)
+
+static int parseopt(int argc, char **argv)
 {
-        printf("hello world!");
+        int optcounter = 0;
+        int c = 0;
+
+        static struct option opt_table[] = {
+                {"mode", required_argument, 0, 'm'},
+                {0, 0, 0, 0}
+        };
+
+        while(1) {
+                c = getopt_long(argc, argv, "m:", opt_table, &optcounter);
+                dbgchr(c);
+
+                if (c == -1)
+                        break;
+
+        }
+
+        
+}
+
+int main(int argc, char **argv)
+{
+        parseopt(argc, argv);
 }
